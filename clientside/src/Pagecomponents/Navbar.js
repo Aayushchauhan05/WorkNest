97b7b94c49ace1319a1d562dc0c4ee2ecba0add2
KARTIK5799 <thekartiksapkal@gmail.com>
2024-05-30 22:03:16 +0530

    Profile was remaining

--- clientside/src/Pagecomponents/Navbar.js
+++ clientside/src/Pagecomponents/Navbar.js
@@ -29,10 +29,14 @@ function Navbar() {
         <div className="flex Login_register justify-evenly w-36">
           {isLoggedIn ? (
             <>
-            <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600" >
+               <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600" >
+            <Link href="/freelancerDashboard/profile">
+         
               
               <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path></svg>
             
+    
+            </Link>
             </div>
              <Button  onClick={handleLogout} className="ml-7 hidden md:block">
              logout
